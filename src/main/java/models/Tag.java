6a2a02d6f1011ac94c844821dc0176e81f54b2f0
BilalChbanat <bilal.chbanat2003@gmail.com>
2024-10-14 13:25:19 +0100

    fixing tag class

---
+++ src/main/java/models/Tag.java
@@ -0,0 +1,49 @@
+package models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tags")
+public class Tag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    public Tag() {}
+
+    public Tag(String name) {
+        this.name = name;
+    }
+
+    public Tag(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Tag{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
